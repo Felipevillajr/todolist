e0a8866181d66f5fa32e69e931766eca144b6a5e
felipevillajr <felipevilla2010@live.com>
2021-07-12 10:37:09 -0400

    added background button textarea to add to list

--- client/src/App.js
+++ client/src/App.js
@@ -3,26 +3,41 @@ import "./App.scss";
 
 let index = 0;
 
-function changeBanner() {
-  [].forEach.call(document.images, function (v, i) {
-    document.images[i].hidden = i !== index;
-  });
-  index = (index + 1) % document.images.length;
-}
-window.onload = function () {
-  setInterval(changeBanner, 1000);
-};
+// function changeBanner() {
+//   [].forEach.call(document.images, function (v, i) {
+//     document.images[i].hidden = i !== index;
+//   });
+//   index = (index + 1) % document.images.length;
+// }
+// window.onload = function () {
+//   setInterval(changeBanner, 1000);
+// };
 
 export default function App() {
-  const [bg, setbg] = useState();
+  // const [bg, setbg] = useState();
 
-  useEffect(() => {});
+  // useEffect(() => {});
+
+  function handleClick(e) {}
 
   return (
     <section id="slideshow">
+      <img
+        id="bg"
+        src="http://localhost:3000/IMGS/skaterdude.jpg"
+        alt="skater background"
+      />
       <div className="todo__body">
         <h1> &#128512; Felipe's To Do List &#129324;</h1>
-        <ol></ol>
+        <ol>
+          <li>shower</li>
+          <li>brush teeth</li>
+        </ol>
+
+        <textarea type="textarea" id="todo__add" />
+        <button id="add__button" onClick={() => handleClick}>
+          Add Item
+        </button>
       </div>
     </section>
   );
